9b2cdc1954ddb12b04ea72953cc0f18579616839
Victor Tran <vicr12345@gmail.com>
2024-04-20 20:10:36 +1000

    Add more info to slides

--- src/slides/i18next.tsx
+++ src/slides/i18next.tsx
@@ -1,7 +1,11 @@
 import { Slide } from "../slide";
+import {useTranslation} from "react-i18next";
 
 export function I18nextSlide() {
+    const {t} = useTranslation();
     return <Slide>
-        .
+        <h1>{t("I18NEXT_TITLE")}</h1>
+        <p>{t("I18NEXT_DESCRIPTION")}</p>
+        <p>{t("I18NEXT_DESCRIPTION_2")}</p>
     </Slide>
 }
