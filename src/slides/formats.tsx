f9162d08ff6e1875fb4af47e46fb5cec6e6f7be1
Victor Tran <vicr12345@gmail.com>
2024-04-25 22:24:12 +1000

    Update number in formats

--- src/slides/formats.tsx
+++ src/slides/formats.tsx
@@ -80,7 +80,7 @@ export function FormatsSlide() {
                     const numberFormat = new Intl.NumberFormat(lang, {maximumFractionDigits: 20});
                     return <Fragment key={lang}>
                         <LanguageIdentifier lang={lang}/>
-                        <span>{numberFormat.format(340000000)}</span>
+                        <span>2³² = {numberFormat.format(4294967296)}</span>
                         <span>π = {numberFormat.format(Math.PI)}</span>
                     </Fragment>;
                 })}
